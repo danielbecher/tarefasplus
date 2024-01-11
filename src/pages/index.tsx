86ff772864dfc3bdea33641fae3f6a939f38a167
Daniel <daniel@becher.com.br>
2024-01-11 08:10:19 -0300

    criando header da aplicação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,34 @@ import heroImg from "../../public/assets/hero.png";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.logoContent}>
-        <Image
-          className={styles.hero}
-          alt="Logo Tarefas Plus"
-          src={heroImg}
-          priority
-        />
-      </div>
-      <h1>Sistema feito para você organizar<br/>seus estudos</h1>
-    </main>
+    <div className={styles.container}>
+      <Head>
+        <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
+      </Head>
 
-  )
+      <main className={styles.main}>
+        <div className={styles.logoContent}>
+          <Image
+            className={styles.hero}
+            alt="Logo Tarefas+"
+            src={heroImg}
+            priority
+          />
+        </div>
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br />
+          seus estudos e terefas
+        </h1>
+
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>+12 posts</span>
+          </section>
+          <section className={styles.box}>
+            <span>+90 comentários</span>
+          </section>
+        </div>
+      </main>
+    </div>
+  );
 }
